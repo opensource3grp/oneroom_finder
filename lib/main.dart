2ddc7f8dcddf2ee7beb6972aad8ef21fa77a1140
user_name <bakeojin@gmail.com>
2024-11-12 21:15:56 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer' as developer;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 초기화
-  await Firebase.initializeApp(); // Firebase 초기화
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,44 +17,285 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Firebase Demo',
-      home: HomeScreen(),
+    return const MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
+// 스플래시 화면
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateBasedOnLoginStatus();
+  }
+
+  Future<void> _navigateBasedOnLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final autoLogin = prefs.getBool('autoLogin') ?? false;
+
+    await Future.delayed(const Duration(seconds: 3)); // 스플래시 화면 표시 시간
+
+    if (autoLogin) {
+      developer.log('자동 로그인 성공');
+      _navigateToHome(); // 자동 로그인 시 홈 화면으로 이동
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginSignupScreen()),
+      );
+    }
+  }
+
+  void _navigateToHome() {
+    // 홈 화면으로 이동하는 코드 작성
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const HomeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 로고 이미지 추가
+            Image.asset('assets/logo.png', width: 100, height: 100),
+            const SizedBox(height: 20),
+            const Text(
+              '원룸알리미',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange),
+            ),
+            const Text(
+              '대학로의 모든 원룸 제공 서비스',
+              style: TextStyle(fontSize: 16, color: Colors.black54),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 로그인/회원가입 화면
+class LoginSignupScreen extends StatefulWidget {
+  const LoginSignupScreen({super.key});
+
+  @override
+  _LoginSignupScreenState createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  bool isLogin = true;
+  String? selectedJob;
+  bool isAutoLogin = false;
+
+  final List<String> jobOptions = ['학생', '공인중개사'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('원룸알리미'),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          width: 300,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (isLogin) ...[
+                const Text(
+                  '안녕하세요\n휴대폰 번호로 로그인해 주세요',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 20),
+                _buildPhoneNumberField(),
+                _buildPasswordField(),
+                _buildAutoLoginCheckbox(),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    developer.log('Login button clicked');
+                    final prefs = await SharedPreferences.getInstance();
+                    prefs.setBool('autoLogin', isAutoLogin);
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                  child: const Text('로그인'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isLogin = false;
+                    });
+                  },
+                  child: const Text('회원가입'),
+                ),
+              ] else ...[
+                const Text(
+                  '안녕하세요\n휴대폰 번호로 회원가입 해주세요',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 20),
+                _buildPhoneNumberField(),
+                _buildPasswordField(),
+                _buildPasswordConfirmField(),
+                _buildTextField('닉네임'),
+                _buildDropdownField('직업 선택', jobOptions, selectedJob, (value) {
+                  setState(() {
+                    selectedJob = value;
+                  });
+                }),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    developer.log('Signup button clicked');
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                  child: const Text('회원가입'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isLogin = true;
+                    });
+                  },
+                  child: const Text('로그인으로 돌아가기'),
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPhoneNumberField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: '휴대폰 번호(숫자만 입력)',
+        border: OutlineInputBorder(),
+      ),
+      keyboardType: TextInputType.phone,
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        decoration: const InputDecoration(
+          labelText: '비밀번호',
+          border: OutlineInputBorder(),
+        ),
+        obscureText: true,
+      ),
+    );
+  }
+
+  Widget _buildPasswordConfirmField() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        decoration: const InputDecoration(
+          labelText: '비밀번호 확인',
+          border: OutlineInputBorder(),
+        ),
+        obscureText: true,
+      ),
+    );
+  }
+
+  Widget _buildDropdownField(String label, List<String> items,
+      String? selectedItem, ValueChanged<String?> onChanged) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
+        ),
+        value: selectedItem,
+        items: items
+            .map((item) => DropdownMenuItem(
+                  value: item,
+                  child: Text(item),
+                ))
+            .toList(),
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAutoLoginCheckbox() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Checkbox(
+          value: isAutoLogin,
+          onChanged: (value) {
+            setState(() {
+              isAutoLogin = value ?? false;
+            });
+          },
+        ),
+        const Text('자동 로그인'),
+      ],
+    );
+  }
+}
+
+// 임시 홈 화면
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Firebase Example')),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('your_collection_name')
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (!snapshot.hasData) {
-            return const Center(child: Text('No data available'));
-          }
-
-          var data = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(data[index]['name']),
-                subtitle: Text(data[index]['email']),
-              );
-            },
-          );
-        },
+      appBar: AppBar(
+        title: const Text('홈 화면'),
+        backgroundColor: Colors.orange,
+      ),
+      body: const Center(
+        child: Text('홈 화면 내용'),
       ),
     );
   }
